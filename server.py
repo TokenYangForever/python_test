50bede35179626438646157df3b185fe47116626
tangyangyang <tangyang22007@126.com>
2018-01-02 18:37:45 +0800

    设置跨域

--- server.py
+++ server.py
@@ -1,6 +1,4 @@
-from flask import Flask
-from flask import request
-from flask import jsonify
+from flask import Flask, request, jsonify, make_response
 from bs4 import BeautifulSoup
 import requests
 
@@ -9,9 +7,6 @@ app = Flask(__name__)
 @app.route('/historyToday', methods=['GET'])
 def historyToday():
 	soup = BeautifulSoup(requests.get("http://www.todayonhistory.com/").content, 'html.parser', from_encoding='utf-8')
-	# 年份
-	moh = list(map(lambda x: x.b.text, soup.find_all('span', 'moh')))
-	# 图片
 	result = []
 	imgDiv = soup.find_all('div', 'pic')
 	for index, iD in enumerate(imgDiv):
@@ -20,8 +15,12 @@ def historyToday():
 			src = temp['data-original']
 			if src[0] == '/':
 				src = 'http://www.todayonhistory.com' + src
-			result.append({'src': src, 'year': moh[index], 'title': temp['alt']})
+			result.append({'src': src, 'year': iD.find_previous_sibling().span.b.text, 'title': temp['alt']})
 
-	return	jsonify({'result': result})
-
+	response = make_response(jsonify({'result': result}))
+	# 设置cors 允许跨域
+	response.headers['Access-Control-Allow-Origin'] = '*'
+	response.headers['Access-Control-Allow-Methods'] = 'POST'
+	response.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
+	return	response
 app.run()
